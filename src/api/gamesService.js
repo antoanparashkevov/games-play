2b53113148a88c3d3549e9ff1650edba8c160340
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-04 17:05:44 +0300

    Made create game request and test it through window obj

--- src/api/gamesService.js
+++ src/api/gamesService.js
@@ -3,7 +3,8 @@ import * as api from './api.js'
 //associative array
 const endpoints = {
     recent: '/data/games?sortBy=_createdOn%20desc&distinct=category',
-    all:'/data/games?sortBy=_createdOn%20desc'
+    all:'/data/games?sortBy=_createdOn%20desc',
+    create:'/data/games'
 }
 
 export async function getRecentGames() {
@@ -12,4 +13,8 @@ export async function getRecentGames() {
 
 export async function getAllGames(){
     return api.get(endpoints.all)
+}
+
+export async function createGame(data){
+    return api.post(endpoints.create,data);
 }
--- src/middleware/render.js
+++ src/middleware/render.js
@@ -1,15 +1,37 @@
-import {render} from '../../node_modules/lit-html/lit-html.js'
+import {html, render} from '../../node_modules/lit-html/lit-html.js'
 
+
+const header = document.getElementById('my-header')
 const root = document.getElementById('main-content')
 
-function ctxRender(content){
-    render(content,root)
+const navTemplate = (user) => html
+    ` <h1><a class="home" href="/">GamesPlay</a></h1>
+    <nav>
+        <a href="/catalog">All games</a>
+        ${user
+                ? html`
+                    <div id="user">
+                        <a href="/create">Create Game</a>
+                        <a href="/logout">Logout</a>
+                    </div>`
+                : html`
+                    <div id="guest">
+                        <a href="/login">Login</a>
+                        <a href="/register">Register</a>
+                    </div>`}
+
+    </nav>`
+
+
+function ctxRender(content) {
+    render(content, root)
 }
 
 
 //function that wil be executed before page handler
-export function addRender(ctx,next){
-    console.log(ctx.user)
+export function addRender(ctx, next) {
+    // console.log(ctx.user)
+    render(navTemplate(ctx.user), header)
     ctx.render = ctxRender;//as reference
     next();
 }

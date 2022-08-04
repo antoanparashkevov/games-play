41e75ed6fc08ded9d4545558a0b47674f15b5707
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-04 14:14:56 +0300

    Create placeholder Home View from our middleware and render the home template

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 import page from '../node_modules/page/page.mjs'
 import {addRender} from "./middleware/render.js";
+import {homePage} from "./views/home.js";
 
 //routing table
 
 //as global middleware
 page(addRender)
 
-page('/', ()=>console.log('home page'))
-page('/catalog', ()=>console.log('catalog page'))
-page('/create', ()=>console.log('create page'))
-page('/details/:id', ()=>console.log('details page'))
-page('/edit/:id', ()=>console.log('edit page'))
-page('/login', ()=>console.log('login page'))
-page('/register', ()=>console.log('register page'))
+page('/', homePage)
+page('/catalog', () => console.log('catalog page'))
+page('/create', () => console.log('create page'))
+page('/details/:id', () => console.log('details page'))
+page('/edit/:id', () => console.log('edit page'))
+page('/login', () => console.log('login page'))
+page('/register', () => console.log('register page'))
 
 page.start();

dfdcba4bf374ad9d1db68613263dc2cd5d8f513f
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-04 15:04:04 +0300

    Rendering all games in catalog dynamically from server

--- src/api/gamesService.js
+++ src/api/gamesService.js
@@ -2,9 +2,14 @@ import * as api from './api.js'
 
 //associative array
 const endpoints = {
-    recent: '/data/games?sortBy=_createdOn%20desc&distinct=category'
+    recent: '/data/games?sortBy=_createdOn%20desc&distinct=category',
+    all:'/data/games?sortBy=_createdOn%20desc'
 }
 
 export async function getRecentGames() {
     return api.get(endpoints.recent)
+}
+
+export async function getAllGames(){
+    return api.get(endpoints.all)
 }

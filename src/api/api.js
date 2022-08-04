1d23bba0d72737cc03efe37a11e150a1bf3e8dfe
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-04 13:37:08 +0300

    Creating Data Service for requests to the server and for user authorization

---
+++ src/api/api.js
@@ -0,0 +1,44 @@
+import {clearUserData, getAccessToken} from "./util.js";
+
+const host = 'localhost:3030'
+
+ async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    }
+    const token = getAccessToken();
+    if (token !== null) {
+        options.headers['X-Authorization'] = token;
+    }
+    if (data !== null) {
+        options.headers['Content-Type'] = 'application/json'
+        options.body = JSON.stringify(data);
+    }
+    try {
+        const response = await fetch(host + url, options)
+
+        if (response.ok !== true) {
+            if (response.status === 403) {//if the token was expired
+                clearUserData();
+            }
+            const error = await response.json();
+            throw new Error(error.message)
+
+        }
+    if(response.status === 204) {//for logout case, the server will return response without body
+        return response;
+
+    }else{
+        return response.json();
+    }
+
+    } catch (error) {
+        alert(error.message)
+        throw error
+    }
+}
+export const get = request.bind(null,'get')
+export const post = request.bind(null,'post')
+export const put = request.bind(null,'put')
+export const del = request.bind(null,'del')
---
+++ src/api/util.js
@@ -0,0 +1,41 @@
+//for user authorization sessions
+
+export function getUserData() {
+    return JSON.parse(localStorage.getItem('user'))//from json to object
+}
+
+export function getAccessToken() {
+    const user = getUserData();
+    if (user !== null) {
+        return user.accessToken
+    } else {
+        return null;
+    }
+}
+
+export function clearUserData() {
+    localStorage.removeItem('user')
+}
+
+export function setUserData(data) {
+    localStorage.setItem('user', JSON.stringify(data))
+}
+
+export function createSubmitHandler(ctx, handler) {//to remove the boiler plate from processing the forms
+    return function (event) {
+        event.preventDefault();
+        const formData = Object.fromEntries(new FormData(event.target))
+
+        //decorator func
+        handler(ctx, formData, event)
+    }
+}
+
+export function parseQueryString(query = '') {
+    return Object.fromEntries(
+        query
+            .split('&')
+            .map(kvp
+                =>
+                kvp.split('=')))//[['key','value],['key','value']]
+}

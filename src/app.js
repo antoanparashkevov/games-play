022f873419d34480cdb3e5c7df361f66a9cde4b9
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-04 13:50:52 +0300

    add all links to the navbar and test it with page.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import page from '../node_modules/page/page.mjs'
 
+//routing table
 page('/', ()=>console.log('it works'))
+page('/catalog', ()=>console.log('catalog page'))
+page('/create', ()=>console.log('create page'))
+page('/logout', ()=>console.log('logout page'))
+page('/login', ()=>console.log('login page'))
+page('/register', ()=>console.log('register page'))
 
 page.start();

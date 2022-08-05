eb64bebefb682e82c5aa972b62b7155d63f7be98
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-05 13:16:15 +0300

    Submitting form for comments

--- src/views/commentForm.js
+++ src/views/commentForm.js
@@ -1,10 +1,11 @@
 import {html,nothing} from '../../node_modules/lit-html/lit-html.js'
 import * as commentsService from '../api/comments.js'
+import {createSubmitHandler} from "../api/util.js";
 
-const commentFormTemplate = ()=> html
+const commentFormTemplate = (onSubmit)=> html
     `<article class="create-comment">
         <label>Add new comment:</label>
-        <form class="form">
+        <form @submit="${onSubmit}" class="form">
             <textarea name="comment" placeholder="Comment......"></textarea>
             <input class="btn submit" type="submit" value="Add Comment">
         </form>
@@ -14,8 +15,15 @@ const commentFormTemplate = ()=> html
 
 export  function commentFormView(ctx){
     if(ctx.user){//only authorized users can type comments.
-        return commentFormTemplate()
+        return commentFormTemplate(createSubmitHandler(ctx,onSubmit))
     }else{
         return nothing
     }
+}
+
+async function onSubmit(ctx,data,event){
+    const gameId = ctx.params.id
+    console.log(gameId)
+    //value from textarea tag
+    console.log(data.comment)
 }
